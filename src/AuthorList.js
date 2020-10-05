f244463dd7219574614e3b8d422cc3d72cdaf4b5
atheen <aatheen.ds@gmail.com>
2020-10-05 11:49:50 +0300

    completed the task

--- src/AuthorList.js
+++ src/AuthorList.js
@@ -2,14 +2,17 @@ import React from "react";
 
 // Components
 import AuthorCard from "./AuthorCard";
+import AuthorDetail from "./AuthorDetail"
+import SearchBar from "./SearchBar"
 
 const AuthorList = props => {
   const authorCards = props.authors.map(author => (
-    <AuthorCard key={author.first_name + author.last_name} author={author} />
+    <AuthorCard key={author.first_name + author.last_name} author={author} select={props.select} />
   ));
 
   return (
     <div className="authors">
+      <SearchBar filterAuthor={props.filterAuthor}/>
       <h3>Authors</h3>
       <div className="row">{authorCards}</div>
     </div>
